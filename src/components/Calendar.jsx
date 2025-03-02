61778ef8f425920055cf419f4bea6b060a200c3e
Aruna Wankhade <wankhadearuna098@gmail.com>
2025-03-02 18:41:14 +0530

    #2 : Implemented basic Sidebar and Navbar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,75 +1,94 @@
-import React, { useEffect, useRef } from "react";
-import { createCalendar, createViewDay, createViewMonthAgenda, createViewMonthGrid, createViewWeek } from "@schedule-x/calendar";
+import React, {useEffect, useRef} from "react";
+import {
+    createCalendar,
+    createViewDay,
+    createViewMonthAgenda,
+    createViewMonthGrid,
+    createViewWeek
+} from "@schedule-x/calendar";
 import "@schedule-x/theme-default/dist/index.css"; // Import theme
 
-import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
-import { createCalendarControlsPlugin } from "@schedule-x/calendar-controls";
-import { createScrollControllerPlugin } from "@schedule-x/scroll-controller";
-import { createEventsServicePlugin } from "@schedule-x/events-service";
-import { createCurrentTimePlugin } from "@schedule-x/current-time";
-import { createEventModalPlugin } from "@schedule-x/event-modal";
-import { createResizePlugin } from "@schedule-x/resize";
+import {createDragAndDropPlugin} from "@schedule-x/drag-and-drop";
+import {createCalendarControlsPlugin} from "@schedule-x/calendar-controls";
+import {createScrollControllerPlugin} from "@schedule-x/scroll-controller";
+import {createEventsServicePlugin} from "@schedule-x/events-service";
+import {createCurrentTimePlugin} from "@schedule-x/current-time";
+import {createEventModalPlugin} from "@schedule-x/event-modal";
+import {createResizePlugin} from "@schedule-x/resize";
 
 const CalendarComp = () => {
-  const initialized = useRef(false);
+    const initialized = useRef(false);
 
-  const calendarRef = useRef(null);
-
-  useEffect(() => {
-    if (!initialized.current && calendarRef.current) {
-      initialized.current = true; 
-      const calendar = createCalendar({
-        selectedDate: new Date().toISOString().split("T")[0],
-        isDark: false,
-        views: [createViewDay(), createViewMonthAgenda(), createViewMonthGrid(), createViewWeek()],
-        
-         weekOptions: {
-          gridHeight: 1000,
-         },
-        calendars: {
-          personal: {
-            colorName: 'personal',
-            lightColors: {
-              main: '#f9d71c',
-              container: '#fff5aa',
-              onContainer: '#594800',
+    const calendarRef = useRef(null);
+    const scrollerStyles = theme => ({
+        '@global': {
+            '*::-webkit-scrollbar': {
+                width: '0.4em'
             },
-            darkColors: {
-              main: '#fff5c0',
-              onContainer: '#fff5de',
-              container: '#a29742',
+            '*::-webkit-scrollbar-track': {
+                '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
             },
-          }
-        },
-        events: [
-          {
-            id: 1,
-            title: 'Coffee with John',
-            start: '2025-03-10 10:05',
-            end: '2025-03-10 10:35',
-          },
-          {
-            id: 2,
-            title: 'Ski trip',
-            start: '2025-03-01',
-            end: '2025-03-02',
-          },
-        ],
-      },
-      [
-        createDragAndDropPlugin(),
-        createCalendarControlsPlugin(),
-        createScrollControllerPlugin(),
-        createEventsServicePlugin(),
-        createCurrentTimePlugin(),
-        createEventModalPlugin(),
-        createResizePlugin(),
-      ])
-      calendar.render(calendarRef.current);
-    }
-  }, []);
+            '*::-webkit-scrollbar-thumb': {
+                backgroundColor: 'rgba(0,0,0,.1)',
+                outline: '1px solid slategrey'
+            }
+        }
+    });
+    useEffect(() => {
+        if (!initialized.current && calendarRef.current) {
+            initialized.current = true;
+            const calendar = createCalendar({
+                    selectedDate: new Date().toISOString().split("T")[0],
+                    isDark: false,
+                    views: [createViewDay(), createViewMonthAgenda(), createViewMonthGrid(), createViewWeek()],
 
-  return <div id="calender" ref={calendarRef} style={{ width: "100%", height: "100%" }} />;
+                    weekOptions: {
+                        gridHeight: 1000,
+                    },
+                    calendars: {
+                        personal: {
+                            colorName: 'personal',
+                            lightColors: {
+                                main: '#f9d71c',
+                                container: '#fff5aa',
+                                onContainer: '#594800',
+                            },
+                            darkColors: {
+                                main: '#fff5c0',
+                                onContainer: '#fff5de',
+                                container: '#a29742',
+                            },
+                        }
+                    },
+                    events: [
+                        {
+                            id: 1,
+                            title: 'Coffee with John',
+                            start: '2025-03-01 08:05',
+                            end: '2025-03-01 12:30',
+                        },
+                        {
+                            id: 2,
+                            title: 'Ski trip',
+                            start: '2025-03-01',
+                            end: '2025-03-01',
+                        },
+                    ],
+                },
+                [
+                    createDragAndDropPlugin(),
+                    createCalendarControlsPlugin(),
+                    createScrollControllerPlugin(),
+                    createEventsServicePlugin(),
+                    createCurrentTimePlugin(),
+                    createEventModalPlugin({}),
+                    createResizePlugin(),
+                ])
+            calendar.render(calendarRef.current);
+        }
+    }, []);
+
+    return <div id="calender" ref={calendarRef} style={{scrollerStyles, width: "100%", height: "90vh"}}/>;
 };
 
 export default CalendarComp;
